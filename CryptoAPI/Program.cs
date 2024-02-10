671c8e6552ed64791aa1b5c2ee894d4a429afd14
Ivy Analisa <ivy.analisa@gmail.com>
2024-02-10 14:41:31 +0100

    add CORS confirguration

--- CryptoAPI/Program.cs
+++ CryptoAPI/Program.cs
@@ -5,10 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+/*
 //Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -17,14 +17,18 @@ if (app.Environment.IsDevelopment())
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
     });
-}
+}*/
 
 
-app.UseStaticFiles();
+
+    app.UseStaticFiles();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 app.Run();
