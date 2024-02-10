a76a5f330df0c65f4621797481039d059b636b14
Ivy Analisa <ivy.analisa@gmail.com>
2024-02-10 14:08:51 +0100

    add index.html

--- CryptoAPI/Program.cs
+++ CryptoAPI/Program.cs
@@ -5,11 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-/* Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,16 +18,9 @@ if (app.Environment.IsDevelopment())
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
     });
 }
-else
-{
-    // For non-development environments (e.g., production), use the AWS Elastic Beanstalk domain for Swagger UI
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("https://encryptionapi-env.eba-tyihg6pz.eu-north-1.elasticbeanstalk.com/swagger/v1/swagger.json", "My API V1");
-    });
-}*/
 
+
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
